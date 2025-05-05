9841505b8566a82a99569d7e74960fa9e87ffdc4
Tiago Lascasas <tiago-antunes@hotmail.com.br>
2025-05-05 08:44:56 -0300

    funcionando

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -5,11 +5,7 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL!,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 
 app.use(express.json());
 
